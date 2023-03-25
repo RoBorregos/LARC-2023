612b23854cfb8c7f2231073ffe36c9f081c7e2b2
JABV75 <a00832948@tec.mx>
2023-03-24 20:43:33 -0600

    dont upload build devel

--- catkin_larc/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/afr2903/larc2023/catkin_larc/src
-# Build directory: /home/afr2903/larc2023/catkin_larc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("simulation")
-subdirs("navigation/robot_description")
--- catkin_larc/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/afr2903/larc2023/catkin_larc/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/afr2903/larc2023/catkin_larc/devel;/home/afr2903/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/afr2903/larc2023/catkin_larc/devel;/home/afr2903/catkin_ws/devel;/opt/ros/noetic' != '' else []
