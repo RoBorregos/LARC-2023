c8d8f9f165f7c5fd36818f76d5da33682c485edf
afr2903 <afr102903@gmail.com>
2023-03-30 14:37:17 -0600

    Intake class created

--- main_controller/Intake.cpp
+++ main_controller/Intake.cpp
@@ -1,5 +1,22 @@
 #include "Intake.h"
 
 Intake::Intake(){
-    
+    pinMode(Constants::kIntakeMotor1A, OUTPUT);
+    pinMode(Constants::kIntakeMotor1B, OUTPUT);
+    pinMode(Constants::kIntakeMotor2A, OUTPUT);
+    pinMode(Constants::kIntakeMotor2B, OUTPUT);
+}
+
+void Intake::pick(){
+    analogWrite(Constants::kIntakeMotor1A, Constants::kIntakePickSpeed);
+    analogWrite(Constants::kIntakeMotor1B, 0);
+    analogWrite(Constants::kIntakeMotor2A, Constants::kIntakePickSpeed);
+    analogWrite(Constants::kIntakeMotor2B, 0);
+}
+
+void Intake::drop(){
+    analogWrite(Constants::kIntakeMotor1A, 0);
+    analogWrite(Constants::kIntakeMotor1B, Constants::kIntakeDropSpeed);
+    analogWrite(Constants::kIntakeMotor2A, 0);
+    analogWrite(Constants::kIntakeMotor2B, Constants::kIntakeDropSpeed);
 }
