53d4163772198b956e08e6ba72bc8d23334e6101
JABV75 <a00832948@tec.mx>
2023-04-19 19:52:56 -0600

    changes

--- Vision/dataset_gen.py
+++ Vision/dataset_gen.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 import random
 
 # Tamaño de la imagen
-for i in range(1500):
+for i in range(1000):
     print(i)
     width = 500
     height = 500
@@ -16,144 +16,187 @@ for i in range(1500):
 
     # Elegir un tamaño y posición aleatoria para la letra
     font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
 
     # Crear objeto de fuente con Arial y el tamaño aleatorio
-    font = ImageFont.truetype("/System/Library/Fonts/Supplemental/Arial.ttf", font_size)
-    
+    #print("aaa")
+    font = ImageFont.truetype("C:\\Windows\\Fonts\\\Arial.ttf", font_size)
     img = Image.new('RGB', (width, height), color = (255, 255, 255))
     
     d = ImageDraw.Draw(img)
 
     # Elegir un tamaño y posición aleatoria para la letra
-    font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
+    text_width, text_height = d.textsize("A", font=font)
+    x = random.randint(0, width - text_width)
+    y = random.randint(0, height - text_width)
 
     # Dibujar la letra "A" en la posición aleatoria
     d.text((x, y), "A", fill=(0, 0, 0), font=font)
 
+    # Generar un ángulo de rotación aleatorio
+    angle = random.randint(-30, 30)
+
+    # Rotar la letra dibujada por el ángulo generado
+    img = img.rotate(angle, expand=True)
+
     # Guardar la imagen
-    img.save("/Users/josebenvenuto/Desktop/letras_dataset/A/imagen_A" + str(i) + ".png")
+    img.save("D:\\UsX\\Escritorio\\letras_dataset3\\A\\imagen_A" + str(i) + ".png")
     
     img = Image.new('RGB', (width, height), color = (255, 255, 255))
     
     d = ImageDraw.Draw(img)
-
+    text_width, text_height = d.textsize("B", font=font)
+    x = random.randint(0, width - text_width)
+    y = random.randint(0, height - text_width)
     # Elegir un tamaño y posición aleatoria para la letra
-    font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
+
 
     # Dibujar la letra "A" en la posición aleatoria
     d.text((x, y), "B", fill=(0, 0, 0), font=font)
 
+    # Generar un ángulo de rotación aleatorio
+    angle = random.randint(-30, 30)
+
+    # Rotar la letra dibujada por el ángulo generado
+    img = img.rotate(angle, expand=True)
+
     # Guardar la imagen
-    img.save("/Users/josebenvenuto/Desktop/letras_dataset/B/imagen_B" + str(i) + ".png")
+    img.save("D:\\UsX\\Escritorio\\letras_dataset3\\B\\imagen_B" + str(i) + ".png")
     
     img = Image.new('RGB', (width, height), color = (255, 255, 255))
     
     d = ImageDraw.Draw(img)
-
+    text_width, text_height = d.textsize("C", font=font)
+    x = random.randint(0, width - text_width)
+    y = random.randint(0, height - text_width)
     # Elegir un tamaño y posición aleatoria para la letra
-    font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
-    
     # Dibujar la letra "A" en la posición aleatoria
     d.text((x, y), "C", fill=(0, 0, 0), font=font)
 
+    # Generar un ángulo de rotación aleatorio
+    angle = random.randint(-30, 30)
+
+    # Rotar la letra dibujada por el ángulo generado
+    img = img.rotate(angle, expand=True)
+
     # Guardar la imagen
-    img.save("/Users/josebenvenuto/Desktop/letras_dataset/C/imagen_C" + str(i) + ".png")
+    img.save("D:\\UsX\\Escritorio\\letras_dataset3\\C\\imagen_C" + str(i) + ".png")
     
     img = Image.new('RGB', (width, height), color = (255, 255, 255))
     
     d = ImageDraw.Draw(img)
-
+    text_width, text_height = d.textsize("D", font=font)
+    x = random.randint(0, width - text_width)
+    y = random.randint(0, height - text_width)
     # Elegir un tamaño y posición aleatoria para la letra
-    font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
-    
     # Dibujar la letra "A" en la posición aleatoria
     d.text((x, y), "D", fill=(0, 0, 0), font=font)
 
+    # Generar un ángulo de rotación aleatorio
+    angle = random.randint(-30, 30)
+
+    # Rotar la letra dibujada por el ángulo generado
+    img = img.rotate(angle, expand=True)
+
     # Guardar la imagen
-    img.save("/Users/josebenvenuto/Desktop/letras_dataset/D/imagen_D" + str(i) + ".png")
+    img.save("D:\\UsX\\Escritorio\\letras_dataset3\\D\\imagen_D" + str(i) + ".png")
     
     img = Image.new('RGB', (width, height), color = (255, 255, 255))
     
     d = ImageDraw.Draw(img)
+    text_width, text_height = d.textsize("E", font=font)
+    x = random.randint(0, width - text_width)
+    y = random.randint(0, height - text_width)
 
-    # Elegir un tamaño y posición aleatoria para la letra
-    font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
     
       # Dibujar la letra "A" en la posición aleatoria
     d.text((x, y), "E", fill=(0, 0, 0), font=font)
 
+    # Generar un ángulo de rotación aleatorio
+    angle = random.randint(-30, 30)
+
+    # Rotar la letra dibujada por el ángulo generado
+    img = img.rotate(angle, expand=True)
+
     # Guardar la imagen
-    img.save("/Users/josebenvenuto/Desktop/letras_dataset/E/imagen_E" + str(i) + ".png")
+    img.save("D:\\UsX\\Escritorio\\letras_dataset3\\E\\imagen_E" + str(i) + ".png")
     
     img = Image.new('RGB', (width, height), color = (255, 255, 255))
     
     d = ImageDraw.Draw(img)
-
-    # Elegir un tamaño y posición aleatoria para la letra
-    font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
+    text_width, text_height = d.textsize("F", font=font)
+    x = random.randint(0, width - text_width)
+    y = random.randint(0, height - text_width)
     
       # Dibujar la letra "A" en la posición aleatoria
     d.text((x, y), "F", fill=(0, 0, 0), font=font)
 
+    # Generar un ángulo de rotación aleatorio
+    angle = random.randint(-30, 30)
+
+    # Rotar la letra dibujada por el ángulo generado
+    img = img.rotate(angle, expand=True)
+
     # Guardar la imagen
-    img.save("/Users/josebenvenuto/Desktop/letras_dataset/F/imagen_F" + str(i) + ".png")
+    img.save("D:\\UsX\\Escritorio\\letras_dataset3\\F\\imagen_F" + str(i) + ".png")
     
     img = Image.new('RGB', (width, height), color = (255, 255, 255))
     
     d = ImageDraw.Draw(img)
+    text_width, text_height = d.textsize("G", font=font)
+    x = random.randint(0, width - text_width)
+    y = random.randint(0, height - text_width)
 
-    # Elegir un tamaño y posición aleatoria para la letra
-    font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
     
       # Dibujar la letra "A" en la posición aleatoria
     d.text((x, y), "G", fill=(0, 0, 0), font=font)
 
+    # Generar un ángulo de rotación aleatorio
+    angle = random.randint(-30, 30)
+
+    # Rotar la letra dibujada por el ángulo generado
+    img = img.rotate(angle, expand=True)
+
     # Guardar la imagen
-    img.save("/Users/josebenvenuto/Desktop/letras_dataset/G/imagen_G" + str(i) + ".png")
+    img.save("D:\\UsX\\Escritorio\\letras_dataset3\\G\\imagen_G" + str(i) + ".png")
     
     img = Image.new('RGB', (width, height), color = (255, 255, 255))
     
     d = ImageDraw.Draw(img)
 
-    # Elegir un tamaño y posición aleatoria para la letra
-    font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
+    text_width, text_height = d.textsize("H", font=font)
+    x = random.randint(0, width - text_width)
+    y = random.randint(0, height - text_width)
+
     
       # Dibujar la letra "A" en la posición aleatoria
     d.text((x, y), "H", fill=(0, 0, 0), font=font)
 
+    # Generar un ángulo de rotación aleatorio
+    angle = random.randint(-30, 30)
+
+    # Rotar la letra dibujada por el ángulo generado
+    img = img.rotate(angle, expand=True)
+
     # Guardar la imagen
-    img.save("/Users/josebenvenuto/Desktop/letras_dataset/H/imagen_H" + str(i) + ".png")
+    img.save("D:\\UsX\\Escritorio\\letras_dataset3\\H\\imagen_H" + str(i) + ".png")
     
     img = Image.new('RGB', (width, height), color = (255, 255, 255))
     
     d = ImageDraw.Draw(img)
 
-    # Elegir un tamaño y posición aleatoria para la letra
-    font_size = random.randint(50, 200)
-    x = random.randint(0, width - font_size)
-    y = random.randint(0, height - font_size)
+    text_width, text_height = d.textsize("I", font=font)
+    x = random.randint(0, width - text_width)
+    y = random.randint(0, height - text_width)
     
       # Dibujar la letra "A" en la posición aleatoria
     d.text((x, y), "I", fill=(0, 0, 0), font=font)
 
+    # Generar un ángulo de rotación aleatorio
+    angle = random.randint(-30, 30)
+
+    # Rotar la letra dibujada por el ángulo generado
+    img = img.rotate(angle, expand=True)
+
     # Guardar la imagen
-    img.save("/Users/josebenvenuto/Desktop/letras_dataset/I/imagen_I" + str(i) + ".png")
+    img.save("D:\\UsX\\Escritorio\\letras_dataset3\\I\\imagen_I" + str(i) + ".png")
     
