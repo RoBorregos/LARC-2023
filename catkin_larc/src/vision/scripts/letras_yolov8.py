20aeff5797d5eb1f3a8fd3a5c72318e04a0e1702
afr2903 <afr102903@gmail.com>
2023-09-27 00:47:23 -0600

    letras_yolov8 jalando

--- catkin_larc/src/vision/scripts/letras_yolov8.py
+++ catkin_larc/src/vision/scripts/letras_yolov8.py
@@ -19,6 +19,8 @@ from vision_utils import *
 
 class letras_yolov8:
         def __init__(self):
+            self.model = ultralytics.YOLO("/home/nvidia/Desktop/LARC-2023/catkin_larc/src/vision/scripts/yolov8n.pt")
+            #self.model = ultralytics.YOLO("larc_yolov8_test4.pt")
             self.bridge = CvBridge()
 
             self.pubdata = rospy.Publisher('vision/letras/info', objectDetectionArray, queue_size=5)
@@ -33,10 +35,9 @@ class letras_yolov8:
             rospy.loginfo("letras_yolov8 initialized")
             self.main()
 
-            self.model = ultralytics.YOLO("larc_yolov8_test4.pt")
             rospy.loginfo("Loading model...")
             rospy.loginfo("letras_yolov8 lectura")
-            yolov8_warmup(self.model, 10, False)
+            self.yolov8_warmup(self.model, 10, False)
 
 
 
@@ -69,7 +70,7 @@ class letras_yolov8:
 
             #prevtime = time.time()
             results = self.model(frame, verbose=False)
-            boxes, confidences, classids = generate_boxes_confidences_classids_v8(results, 0.65)
+            boxes, confidences, classids = self.generate_boxes_confidences_classids_v8(results, 0.65)
             #print(f"Prediction done in {time.time() - prevtime} seconds")
             # Draw results
             bb = []
@@ -168,11 +169,13 @@ class letras_yolov8:
             # create an empty frame to warmup the model
             for i in range(repetitions):
                 warmupFrame = np.zeros((360, 640, 3), dtype=np.uint8)
-                model.predict(source=warmupFrame, verbose=verbose)
+                self.model.predict(source=warmupFrame, verbose=verbose)
             rospy.loginfo(f"Model warmed up in {time.time() - startTime} seconds")
 
         def main(self):
             rospy.logwarn("Starting listener")
+            #rospy.logwarn("aqui mamo")
+
             rospy.init_node('detector_letras', anonymous=True)
             rate = rospy.Rate(10)
             try:
@@ -190,59 +193,59 @@ if __name__ == '__main__':
 
 #--------------------------------------------------------------------------------------------------------------
 
-def generate_boxes_confidences_classids_v8(outs, threshold):
-		boxes = []
-		confidences = []
-		classids = []
+# def generate_boxes_confidences_classids_v8(outs, threshold):
+# 		boxes = []
+# 		confidences = []
+# 		classids = []
 
-		for out in outs:
-				for box in out.boxes:
-					x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
-					class_id = box.cls[0].item()
-					prob = round(box.conf[0].item(), 2)
-					if prob > threshold:
-						# Append to list
-						boxes.append([x1, y1, x2-x1, y2-y1])
-						confidences.append(float(prob))
-						classids.append(class_id)
+# 		for out in outs:
+# 				for box in out.boxes:
+# 					x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
+# 					class_id = box.cls[0].item()
+# 					prob = round(box.conf[0].item(), 2)
+# 					if prob > threshold:
+# 						# Append to list
+# 						boxes.append([x1, y1, x2-x1, y2-y1])
+# 						confidences.append(float(prob))
+# 						classids.append(class_id)
 	
-		return boxes, confidences, classids
+# 		return boxes, confidences, classids
 
-def yolov8_warmup(model, repetitions=1, verbose=False):
-    # Warmup model
-    startTime = time.time()
-    # create an empty frame to warmup the model
-    for i in range(repetitions):
-        warmupFrame = np.zeros((360, 640, 3), dtype=np.uint8)
-        model.predict(source=warmupFrame, verbose=verbose)
-    print(f"Model warmed up in {time.time() - startTime} seconds")
+# def yolov8_warmup(model, repetitions=1, verbose=False):
+#     # Warmup model
+#     startTime = time.time()
+#     # create an empty frame to warmup the model
+#     for i in range(repetitions):
+#         warmupFrame = np.zeros((360, 640, 3), dtype=np.uint8)
+#         model.predict(source=warmupFrame, verbose=verbose)
+#     print(f"Model warmed up in {time.time() - startTime} seconds")
     
-#model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # or yolov5n - yolov5x6, custom
-print("Loading model...")
-prevtime = time.time()
-model = ultralytics.YOLO("larc_yolov8_test4.pt")
-yolov8_warmup(model, 10, False)
-print(f"Model loaded in {time.time() - prevtime} seconds")
+# #model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # or yolov5n - yolov5x6, custom
+# print("Loading model...")
+# prevtime = time.time()
+# model = ultralytics.YOLO("larc_yolov8_test4.pt")
+# yolov8_warmup(model, 10, False)
+# print(f"Model loaded in {time.time() - prevtime} seconds")
 
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
+# cap = cv2.VideoCapture(0)
+# ret, frame = cap.read()
 
-while ret:
-    prevtime = time.time()
-    results = model(frame, verbose=False)
-    boxes, confidences, classids = generate_boxes_confidences_classids_v8(results, 0.65)
-    print(f"Prediction done in {time.time() - prevtime} seconds")
-    # Draw results
-    for i in range(len(boxes)):
-        x, y, w, h = boxes[i]
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        cv2.putText(frame, str(confidences[i]), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-        class_name = model.model.names[classids[i]]
-        cv2.putText(frame, str(class_name), (x, y-30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-    cv2.imshow("frame", frame)
-    ret, frame = cap.read()
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+# while ret:
+#     prevtime = time.time()
+#     results = model(frame, verbose=False)
+#     boxes, confidences, classids = generate_boxes_confidences_classids_v8(results, 0.65)
+#     print(f"Prediction done in {time.time() - prevtime} seconds")
+#     # Draw results
+#     for i in range(len(boxes)):
+#         x, y, w, h = boxes[i]
+#         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+#         cv2.putText(frame, str(confidences[i]), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+#         class_name = model.model.names[classids[i]]
+#         cv2.putText(frame, str(class_name), (x, y-30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+#     cv2.imshow("frame", frame)
+#     ret, frame = cap.read()
+#     if cv2.waitKey(1) & 0xFF == ord("q"):
+#         break
 
-cap.release()
-#cv2.destroyAllWindows()
+# cap.release()
+# #cv2.destroyAllWindows()
